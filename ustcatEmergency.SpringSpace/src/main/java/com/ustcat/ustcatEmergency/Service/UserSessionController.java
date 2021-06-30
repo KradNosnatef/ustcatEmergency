c9c073cb0b157b57d0cdfa551a9be070ab6da2e6
fuqianshan <fuqianshan@foxmail.com>
2021-06-30 23:48:19 +0800

    部署基础页面（后端）

--- ustcatEmergency.SpringSpace/src/main/java/com/ustcat/ustcatEmergency/Service/UserSessionController.java
+++ ustcatEmergency.SpringSpace/src/main/java/com/ustcat/ustcatEmergency/Service/UserSessionController.java
@@ -10,7 +10,9 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class UserSessionController {
     @Autowired
     UserService userService;
