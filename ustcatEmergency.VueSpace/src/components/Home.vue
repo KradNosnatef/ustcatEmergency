bddef7e3e5794992f97e814dd68c8f42bec9c92e
KradNosnatef <fuqianshan@foxmail.com>
2021-06-30 23:47:42 +0800

    部署基础页面

---
+++ ustcatEmergency.VueSpace/src/components/Home.vue
@@ -0,0 +1,36 @@
+<template>
+	<div class="maindiv">
+		首页
+	</div>
+</template>
+
+<script>
+	import jsonp from "jsonp"
+	export default {
+		data() {
+			return {
+			}
+		},
+		methods: {
+		},
+		components: {
+		},
+		mounted: function() {
+		}
+	}
+</script>
+
+<style>
+	.maindiv {
+		height: auto;
+		border: 1px #41B883;
+		box-shadow: 10px 10px 5px #888888;
+	}
+
+	.loginbox {
+		height: 50px;
+		position: absolute;
+		right: 32px;
+		border: 1px #41B883;
+	}
+</style>
