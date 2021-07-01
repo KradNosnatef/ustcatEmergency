8db54e73728fd8b282f2453b2721ebc49ffe6f97
fuqianshan <fuqianshan@foxmail.com>
2021-07-01 09:17:10 +0800

    解决一个狗币问题

--- ustcatEmergency.VueSpace/vue.config.js
+++ ustcatEmergency.VueSpace/vue.config.js
@@ -1,9 +1,11 @@
 module.exports = {
     devServer: {
+        //host: '192.168.3.14',
 		disableHostCheck:true,
         open: false,
         port: 8080,
         https: false,
+		//sockHost: '192.168.3.14:8080',
         //以上的ip和端口是我们本机的;下面为需要跨域的
 		proxy:{
 			'/Xapi':{
